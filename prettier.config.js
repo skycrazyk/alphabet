a978096a919e56162c32967da6d0e63e7e8687db
Dmitry Karyakin <skycrazyk@gmail.com>
2022-10-01 23:23:13 +0200

    first

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 100,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+}
---
+++ src/alphabets.ts
@@ -0,0 +1,131 @@
+type Letter = {
+    upper: string
+    lower: string
+}
+
+export const alphabet: Letter[] = [
+    {
+        upper: 'А',
+        lower: 'а',
+    },
+    {
+        upper: 'Б',
+        lower: 'б',
+    },
+    {
+        upper: 'В',
+        lower: 'в',
+    },
+    {
+        upper: 'Г',
+        lower: 'г',
+    },
+    {
+        upper: 'Д',
+        lower: 'д',
+    },
+    {
+        upper: 'Е',
+        lower: 'е',
+    },
+    {
+        upper: 'Ё',
+        lower: 'ё',
+    },
+    {
+        upper: 'Ж',
+        lower: 'ж',
+    },
+    {
+        upper: 'З',
+        lower: 'з',
+    },
+    {
+        upper: 'И',
+        lower: 'и',
+    },
+    {
+        upper: 'К',
+        lower: 'к',
+    },
+    {
+        upper: 'Л',
+        lower: 'л',
+    },
+    {
+        upper: 'М',
+        lower: 'м',
+    },
+    {
+        upper: 'Н',
+        lower: 'н',
+    },
+    {
+        upper: 'О',
+        lower: 'о',
+    },
+    {
+        upper: 'П',
+        lower: 'п',
+    },
+    {
+        upper: 'Р',
+        lower: 'р',
+    },
+    {
+        upper: 'С',
+        lower: 'с',
+    },
+    {
+        upper: 'Т',
+        lower: 'т',
+    },
+    {
+        upper: 'У',
+        lower: 'у',
+    },
+    {
+        upper: 'Ф',
+        lower: 'ф',
+    },
+    {
+        upper: 'Х',
+        lower: 'х',
+    },
+    {
+        upper: 'Ц',
+        lower: 'ц',
+    },
+    {
+        upper: 'Ш',
+        lower: 'ш',
+    },
+    {
+        upper: 'Щ',
+        lower: 'щ',
+    },
+    {
+        upper: 'Ь',
+        lower: 'ь',
+    },
+    {
+        upper: 'Ъ',
+        lower: 'ъ',
+    },
+    {
+        upper: 'Ы',
+        lower: 'ы',
+    },
+    {
+        upper: 'Э',
+        lower: 'э',
+    },
+    {
+        upper: 'Ю',
+        lower: 'ю',
+    },
+    {
+        upper: 'Я',
+        lower: 'я',
+    },
+]
