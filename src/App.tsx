8369f68106232022c9a0c1d37b0a96afb57359d0
Dmitry Karyakin <skycrazyk@gmail.com>
2022-10-01 23:45:05 +0200

    test sound

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import s from './App.module.css'
 import {alphabet} from './alphabets'
 
 function App() {
+    const sound = new Audio('/sounds/alphabet/А/bell.mp3')
+
     return (
         <div className={s.list}>
             {alphabet.map(l => (
-                <div key={l.upper} className={s.item}>
+                <div key={l.upper} className={s.item} onClick={() => sound.play()}>
                     {l.upper}
                 </div>
             ))}
