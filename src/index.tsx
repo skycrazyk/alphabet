d01bf512c09de4baa80ab05dd74e23640de35794
Dmitry Karyakin <skycrazyk@gmail.com>
2022-10-19 21:03:04 +0500

    Исправлен basename

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = createRoot(container)
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App />
             </Provider>
