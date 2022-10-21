d36022f8bf39dc71ac7015dd73795855f5377afc
Dmitry Karyakin <skycrazyk@gmail.com>
2022-10-21 20:59:52 +0500

    Добавлена кнопка для тренировки

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import {useNavigate, generatePath} from 'react-router-dom'
+import cn from 'classnames'
 import s from './Home.module.css'
 import {alphabet, routes} from '../../utils'
 
@@ -6,12 +7,20 @@ export function Home() {
     const navigate = useNavigate()
     return (
         <div className={s.home}>
-            <button
-                onClick={() => navigate(generatePath(routes.abc, {letter: alphabet[0].upper}))}
-                className={s.start}
-            >
-                Старт
-            </button>
+            <div className={s.menu}>
+                <button
+                    onClick={() => navigate(generatePath(routes.abc, {letter: alphabet[0].upper}))}
+                    className={cn(s.start, s.btn)}
+                >
+                    Знакомство
+                </button>
+                <button
+                    onClick={() => navigate(generatePath(routes.abc, {letter: alphabet[0].upper}))}
+                    className={cn(s.training, s.btn)}
+                >
+                    Тренировка
+                </button>
+            </div>
         </div>
     )
 }
