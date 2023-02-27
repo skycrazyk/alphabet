b757dd90682750b830e89f3debdb1852a73220e1
Dmitry Karyakin <skycrazyk@gmail.com>
2023-02-27 20:59:35 +0500

    start mass load

--- src/store/reducers/assets.ts
+++ src/store/reducers/assets.ts
@@ -1,10 +1,51 @@
-import {createSlice} from '@reduxjs/toolkit'
-import {LetterType} from '../../utils'
+import {parallelLimit, AsyncResultCallback} from 'async'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import type {LetterType} from '../../utils'
+import {alphabet} from '../../utils'
 
-export type LetterAccets = Pick<LetterType, 'upper' | 'lower'> & {
+type LetterAccet = Pick<LetterType, 'upper' | 'lower'> & {
     words: {
         text: string
-        file: Blob
+        sound: Blob
+        image: Blob
     }[]
-    image: Blob
 }
+
+type Assets = {
+    alphabet: LetterAccet[]
+}
+
+export const assetsApi = createApi({
+    reducerPath: 'assetsApi',
+    baseQuery: fetchBaseQuery({baseUrl: '/'}),
+    endpoints: builder => ({
+        getAssets: builder.query<Assets, void>({
+            async queryFn(arg, api, extraOptions, baseQuery) {
+                const assets = {} as Assets
+
+                const alphabetRequests = alphabet.reduce((acc, letter, idx) => {
+                    // const imageCb = () =>
+                    return acc
+                }, [] as ((cb: AsyncResultCallback<any>) => void)[])
+
+                await parallelLimit([], 1)
+
+                return {data: assets}
+                // const randomVal = Math.random()
+                // if (randomVal < 0.45) {
+                //     return {data: 'heads'}
+                // }
+                // if (randomVal < 0.9) {
+                //     return {data: 'tails'}
+                // }
+                // return {
+                //     error: {
+                //         status: 500,
+                //         statusText: 'Internal Server Error',
+                //         data: "Coin landed on it's edge!",
+                //     },
+                // }
+            },
+        }),
+    }),
+})
